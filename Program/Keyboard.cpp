8e5fa8aac8061fec03c22e938c67766526b897f1
Domir07 <weponmaster990@gmail.com>
2024-10-28 21:45:17 +0900

    (2024.10.28) 연산자 오버로딩

---
+++ Program/Keyboard.cpp
@@ -0,0 +1,16 @@
+#include "Keyboard.h"
+
+Keyboard::Keyboard()
+{
+	cout << "Create Ketboard" << endl;
+}
+
+void Keyboard::OnEnter()
+{
+	cout << "Keyboard Enter" << endl;
+}
+
+Keyboard::~Keyboard()
+{
+	cout << "Release Keyboard" << endl;
+}
