bde7376b5c4b7a94e1448b874246a86253061bec
Domir07 <weponmaster990@gmail.com>
2024-10-31 21:46:08 +0900

    (2024.10.31) 업 캐스팅

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -8,13 +8,34 @@ class List
 {
 private:
     int size;
+    int index;
 
     T* pointer;
 
 public:
     List(int size)
     {
+        index = 0;
+
         this->size = size;
+
+        pointer = new T[this->size];
+    }
+    void Add(T data)
+    {
+        if (index >= size) // index가 size보다 크거나 같다면
+        {
+            cout << "List is Full" << endl;
+        }
+        else
+        {
+            pointer[index++] = data;
+        }
+    }
+
+    T & operator [] (int index) // 출력 처리
+    {
+        return pointer[index];
     }
 
     ~List()
@@ -23,6 +44,23 @@ public:
     }
 };
 
+template<typename T>
+bool Same(T x, T y)
+{
+    return x == y;
+}
+
+template<>
+bool Same <const char*>(const char* x, const char* y)
+{
+    cout << "템플릿 특수화" << endl;
+
+    int first = strlen(x);
+    int second = strlen(y);
+
+    return first == second;
+}
+
 int main()
 {
 #pragma region 템플릿
@@ -30,9 +68,40 @@ int main()
     // 타입들을 가질 수 있는 기술에 중점을 두어 재사용을 높일 수
     // 있는 기능입니다.
 
-    List<int> list(5);
+    // List<int> list(5);
+    // 
+    // list.Add(10);
+    // list.Add(20);
+    // list.Add(30);
+    // list.Add(40);
+    // list.Add(50);
+    // 
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "" << list[i] << endl;
+    // }
+
 
 #pragma endregion
+
+#pragma region 템플릿 특수화
+    // 특정 자료형에 대해 다르게 처리하고 싶을 경우
+    // 특정한 자료형만 다른 형식으로 동작시키는 템플릿 기능입니다.
+
+    // cout << Same('A', 'B') << endl;
+    // cout << Same(10, 10) << endl;
+    // cout << Same(10.5, 10.7) << endl;
+    // cout << Same("Apple", "Korea") << endl;
+
+#pragma endregion
+
+#pragma region 업 캐스팅
+    // 하위 클래스의 정보를 담을 수 있는 객체에 상위 클래스의
+    // 자료형을 부여하여, 상위 클래스처럼 사용할 수 있는 기능입니다.
+
+#pragma endregion
+
+
 
     return 0;
 }
